fddf2d19fdf595fa8356a7c31d6613676b04781e
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-03-29 11:41:50 +0100

    Create PinToStartMenu.py

---
+++ PinToStartMenu.py
@@ -0,0 +1,18 @@
+import winshell
+from win32com.client import Dispatch
+
+# get the path to the Start Menu folder
+start_menu = winshell.startup()
+
+# get the list of all programs installed on the system
+programs = winshell.programs()
+
+# create a Windows Shell object
+shell = Dispatch('WScript.Shell')
+
+# loop through each program and create a shortcut in the Start Menu folder
+for program in programs:
+    shortcut = shell.CreateShortcut(f'{start_menu}\\{program}.lnk')
+    shortcut.TargetPath = program
+    shortcut.WorkingDirectory = program
+    shortcut.Save()
